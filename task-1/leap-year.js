7dc522477247a60e209d6b07d3970845746988e7
Halyna Romanyshyn <halyna.romanyshyn@gmail.com>
2026-01-21 01:01:18 +0100

    Complete week 2 tasks 1-3

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -1,9 +1,17 @@
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 const prompt = promptSync();
 
+const input_year = prompt("Enter a year(1-9999):  ");
+const year = Number(input_year);
 
-// Write your code here
-// Guidance:
-// Step 1: prompt the user to enter a year
-// Step 2: convert the user input to a number so we can perform calculations
-// Step 3: Implement the logic
+if (!Number.isInteger(year) || year < 1 || year > 9999) {
+  console.log("Invalid year!");
+} else {
+  const isLeap = year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+
+  if (isLeap) {
+    console.log("Yes, " + year + " is a leap year");
+  } else {
+    console.log("No, " + year + " is not a leap year");
+  }
+}
--- task-2/login.js
+++ task-2/login.js
@@ -3,10 +3,37 @@ import { showMessage } from './app.js';
 
 let incorrectAttempts = 0;
 
+function errorMessage(message) {
+  showMessage(message, "error");
+}
+
+function successMessage(message) {
+  showMessage(message, "success");
+}
+
 function onLogin(username, password) {
-  // Write your code here.
-  // Use the variables 'username' and 'password' to access the input values
-  // Use incorrectAttempts to track the number of failed attempts
+    if (incorrectAttempts >= 4) {
+    errorMessage("Login blocked: Too many incorrect attempts", "error");
+    return;
+  }
+
+  // 2) check valid credentials
+  const isValid =
+    (username === "admin" && password === "Hack1234") ||
+    (username === "user" && password === "7654321");
+
+  if (isValid) {
+    successMessage("Logged in successfully");
+  } else {
+    incorrectAttempts += 1;
+
+    // after increment, if now blocked -> show blocked (requirement 3+4)
+    if (incorrectAttempts >= 4) {
+      errorMessage("Login blocked: Too many incorrect attempts", "error");
+    } else {
+      errorMessage("Incorrect credentials", "error");
+    }
+  }
 }
 
 // Do not change the line below
